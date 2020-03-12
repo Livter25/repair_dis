741a0918c71e743bfd872af7861e44cf1f65155f
Pasha <bl4ckbeer@yandex.ru>
2020-03-12 19:40:20 +0500

    add: js script in modal

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const modal = document.querySelector('.modal')
+    const modalbtn = document.querySelectorAll('[data-toggle=modal]');
+    const closbtn = document.querySelector('.modal__close')
+    const switchModal = () => {
+        modal.classList.toggle('modal--visible')
+
+    };
+    
+    modalbtn.forEach(element => {
+        element.addEventListener('click', switchModal);
+        
+    });
+
+    closbtn.addEventListener('click', switchModal);
+    
+    
+  });
