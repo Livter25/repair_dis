d5e592c3f49dda7493038c5b8c1688d7103895e3
Pasha <bl4ckbeer@yandex.ru>
2020-03-13 12:27:51 +0500

    make: jq and scroll down

--- js/main.js
+++ js/main.js
@@ -1,77 +1,52 @@
-// $(document).ready(function () {
-//     var modal = $('.modal'),
-//         modalbtn = $('[data-toggle=modal]'),
-//         closebtn = $('.modal__close')
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalbtn = $('[data-toggle=modal]'),
+        closebtn = $('.modal__close')
 
-//     modalbtn.on('click', function() {
-//         $(modal).toggleClass('modal--visible');
+    modalbtn.on('click', function() {
+        $(modal).toggleClass('modal--visible');
         
-//     } )
-//     closebtn.on('click', function() {
-//         $(modal).toggleClass('modal--visible');
+    } )
+    closebtn.on('click', function() {
+        $(modal).toggleClass('modal--visible');
         
-//     } )
+    } )
 
 
-//     $(window).scroll(function(){
-//         if ($(this).scrollTop() > 100) {
-//             $('.skroll-up').fadeIn();
-//         } else {
-//             $('.skroll-up').fadeOut();
-//         }
-//         });
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.skroll-up').fadeIn();
+        } else {
+            $('.skroll-up').fadeOut();
+        }
+        });
           
-//         $('.skroll-up').click(function(){
-//         $("html, body").animate({ scrollTop: 0 }, 3000);
-//         return false;
-        
-//     });
-// });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function(event) { 
-    const modal = document.querySelector('.modal')
-    const modalbtn = document.querySelectorAll('[data-toggle=modal]');
-    const closbtn = document.querySelector('.modal__close')
-    const switchModal = () => {
-        modal.classList.toggle('modal--visible')
-
-    };
-    
-    modalbtn.forEach(element => {
-        element.addEventListener('click', switchModal);
+        $('.skroll-up').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 3000);
+        return false;
         
     });
-
-    closbtn.addEventListener('click', switchModal);
-
-    document.addEventListener('keydown', function (e) {
-        switch(e.keyCode){
-            case 27:
-                switchModal();
-        }   
-    });
-
-
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
