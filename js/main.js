9d1030317eef5b0d58f54867e61350084c2b418f
Pasha <bl4ckbeer@yandex.ru>
2020-03-13 23:05:51 +0500

    add:slaid and block project

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     var modal = $('.modal'),
         modalbtn = $('[data-toggle=modal]'),
         closebtn = $('.modal__close')
+        
 
     modalbtn.on('click', function() {
         $(modal).toggleClass('modal--visible');
@@ -12,22 +13,36 @@ $(document).ready(function () {
         
     } )
 
-    $('.skroll-up').fadeOut();
 
-
-    $(window).scroll(function(){
-        if ($(this).scrollTop() > 100) {
-            $('.skroll-up').fadeIn();
-        } else {
-            $('.skroll-up').fadeOut();
-        }
-        });
           
         $('.skroll-up').click(function(){
         $("html, body").animate({ scrollTop: 0 }, 3000);
         return false;
         
     });
+
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+          },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+
+      });
+
+      var next = $('.swiper-button-next');
+      var prev = $('.swiper-button-prev');
+      var bullets = $('.swiper-pagination');
+
+      next.css('left', prev.width() + 10 + bullets.width() + 10)
+      bullets.css('left', prev.width() + 10 )
+    
 });
 
 
